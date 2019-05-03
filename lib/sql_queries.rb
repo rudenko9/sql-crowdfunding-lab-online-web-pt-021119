78563dc95828246e26efa0e1eb30cc6c0d4d508b
Nataliya R <n.rudenko@gmail.com>
2019-05-03 02:20:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,13 +23,13 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-#"SELECT projects.title, SUM(pledges.amount)-projects.funding_goal
- #  FROM projects
-  # JOIN pledges
-   #ON projects.id = pledges.project_id
-   #GROUP BY projects.title
-   #HAVING SUM(pledges.amount) >= projects.funding_goal;"
-"SELECT project.title, (SUM(pledges.amount) - projects.funding_goal) AS total amount FROM projects INNER JOIN pledges ON projects.id = pledges.project id GROUP BY projects.title HAVING total_amount >= 0"
+"SELECT projects.title, SUM(pledges.amount)-projects.funding_goal
+   FROM projects
+   JOIN pledges
+   ON projects.id = pledges.project_id
+   GROUP BY projects.title
+   HAVING SUM(pledges.amount) >= projects.funding_goal;"
+
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
